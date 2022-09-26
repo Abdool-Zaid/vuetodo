f788b1c631ede09c698b621892ae0a9593c91a72
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-09-26 22:04:58 +0200

    'update'

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,40 @@
       v-model="drawer"
       app
     >
-      <!--  -->
+    <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title >
+           the journey of a thousand miles 
+          </v-list-item-title>
+          <v-list-item-subtitle>
+           begins with a single step
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.to"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
+
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -14,13 +46,19 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
+    data: () => ({ 
+      drawer: null,
+      items: [
+          { title: 'steps', icon: 'mdi-clipboard-flow-outline' , to:"/"},
+          { title: 'About', icon: 'mdi-book-information-variant', to:"/about" },
+        ],
+     }),
   }
 </script>
